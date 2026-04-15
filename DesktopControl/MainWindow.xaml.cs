64e5b823a1971f83ea5a4339a21f588de92fe797
Szymon Sowinski <sowinski.szymon@zstz-radzymin.pl>
2026-04-15 14:51:38 +0200

    Update wyswietlania

--- DesktopControl/MainWindow.xaml.cs
+++ DesktopControl/MainWindow.xaml.cs
@@ -1,29 +1,28 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Threading.Tasks;
+using System.Windows;
+
 namespace DesktopControl
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
+    public partial class MainWindow : Window
+    {
         [DllImport("iphlpapi.dll", ExactSpelling = true)]
         public static extern int SendARP(int destIp, int srcIp, byte[] macAddr, ref int physicalAddrLen);
-        ObservableCollection<Komputer> komputery = new ObservableCollection<Komputer>();
+
+        public ObservableCollection<Komputer> Komputery { get; set; } = new ObservableCollection<Komputer>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
         private string GetLocalIPAddress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
@@ -31,57 +30,48 @@ namespace DesktopControl
             foreach (var ip in host.AddressList)
             {
                 if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
                     return ip.ToString();
-                }
             }
 
             throw new Exception("Brak IP");
         }
+
         private string GetHostName(string ip)
         {
-            try
-            {
-                var host = Dns.GetHostEntry(ip);
-                return host.HostName;
-            }
-            catch
-            {
-                return "Unknown";
-            }
+            try { return Dns.GetHostEntry(ip).HostName; }
+            catch { return "Unknown"; }
         }
+
         private string GetMacAddress(string ip)
         {
             try
             {
                 byte[] macAddr = new byte[6];
-                int length = macAddr.Length;
+                int len = macAddr.Length;
 
                 int dest = BitConverter.ToInt32(IPAddress.Parse(ip).GetAddressBytes(), 0);
 
-                SendARP(dest, 0, macAddr, ref length);
+                SendARP(dest, 0, macAddr, ref len);
 
-                string mac = BitConverter.ToString(macAddr);
-                return mac;
+                return BitConverter.ToString(macAddr);
             }
             catch
             {
                 return "Unknown";
             }
         }
-        public MainWindow()
-		{
-			InitializeComponent();
-            DataContext = this;
-        }
 
         private async void WykryjKomputery(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Wykrywanie komputerów w sieci. Proszę czekać...", "Wykrywanie");
-            komputery.Clear();
+            Komputery.Clear();
+
             string localIP = GetLocalIPAddress();
             string subnet = localIP.Substring(0, localIP.LastIndexOf('.') + 1);
+
             List<Task> tasks = new List<Task>();
+            object lockObj = new object();
+            HashSet<string> found = new HashSet<string>();
+
             for (int i = 1; i < 255; i++)
             {
                 string ip = subnet + i;
@@ -90,44 +80,38 @@ namespace DesktopControl
                 {
                     try
                     {
-                        Ping ping = new Ping();
-                        PingReply reply = await ping.SendPingAsync(ip, 200);
-
-                        if (reply.Status == IPStatus.Success)
+                        using (Ping ping = new Ping())
                         {
-                            string hostname = GetHostName(ip);
-                            string mac = GetMacAddress(ip);
+                            var reply = await ping.SendPingAsync(ip, 200);
 
-                            Application.Current.Dispatcher.Invoke(() =>
+                            if (reply.Status == IPStatus.Success)
                             {
-                                komputery.Add(new Komputer(
+                                lock (lockObj)
+                                {
+                                    if (!found.Add(ip))
+                                        return;
+                                }
+
+                                var komputer = new Komputer(
                                     ip,
-                                    mac,
-                                    hostname,
+                                    GetMacAddress(ip),
+                                    GetHostName(ip),
                                     true,
                                     DateTime.Now
-                                ));
-                            });
+                                );
+
+                                Dispatcher.Invoke(() =>
+                                {
+                                    Komputery.Add(komputer);
+                                });
+                            }
                         }
                     }
                     catch { }
                 }));
             }
-            StringBuilder sb = new StringBuilder();
 
-            foreach (var komputer in komputery)
-            {
-                sb.AppendLine($"IP: {komputer.IP}");
-                sb.AppendLine($"MAC: {komputer.MAC}");
-                sb.AppendLine($"Host: {komputer.Hostname}");
-                sb.AppendLine($"Online: {komputer.Onl}");
-                sb.AppendLine("----------------------");
-            }
-
-            MessageBox.Show(sb.ToString(), "Wykryte komputery");
             await Task.WhenAll(tasks);
-
-
         }
     }
 }
