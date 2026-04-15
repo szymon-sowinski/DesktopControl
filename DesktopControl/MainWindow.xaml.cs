3e8d1f63c3c04b650471dc2fef528951bdaeb96a
PanJan21 <wojtek.lukasik123@gmail.com>
2026-04-15 12:33:20 +0200

    Usunięcie checkboxów oraz zablokowanie edycji statusu w DataGrid
    - Usunięto kolumnę checkboxów (zaznaczanie komputerów)
    - Zmieniono status z edytowalnego TextBox na tylko do odczytu TextBlock
    - Poprawiono przejrzystość interfejsu
    - Zaktualizowano dolny panel (usunięto funkcję zaznaczania)

--- DesktopControl/MainWindow.xaml.cs
+++ DesktopControl/MainWindow.xaml.cs
@@ -11,14 +11,41 @@ using System.Windows.Shapes;
 
 namespace DesktopControl
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public class Computer
+	{
+		public bool IsSelected { get; set; }
+		public string Mac { get; set; }
+		public string Ip { get; set; }
+		public string Error { get; set; }
+	}
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var computers = new List<Computer>
+			{
+				new Computer
+				{
+					IsSelected = false,
+					Mac = "00:1A:2B:3C:4D:5E",
+					Ip = "192.168.0.255",
+					Error = "OK"
+				},
+				new Computer
+				{
+					IsSelected = true,
+					Mac = "AA:BB:CC:DD:EE:FF",
+					Ip = "192.168.0.255",
+					Error = "Offline"
+				}
+			};
+
+			ComputersGrid.ItemsSource = computers;
+		}
+	}
 }
