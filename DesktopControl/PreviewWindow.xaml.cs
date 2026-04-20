2e446fab8ff670c322fa318e7350d179c2837d0e
Eryk <Admin@DESKTOP-2TOSSPE>
2026-04-20 10:30:52 +0200

    dodanie agenta i widoku podglądu

---
+++ DesktopControl/PreviewWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace DesktopControl
+{
+    public partial class PreviewWindow : Window
+    {
+        public PreviewWindow(string ip)
+        {
+            InitializeComponent();
+            Connect(ip);
+        }
+
+        private async void Connect(string ip)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                await client.ConnectAsync(ip, 5000);
+
+                var stream = client.GetStream();
+
+                while (true)
+                {
+                    byte[] lenBytes = new byte[4];
+                    await stream.ReadAsync(lenBytes, 0, 4);
+                    int len = BitConverter.ToInt32(lenBytes, 0);
+
+                    byte[] data = new byte[len];
+                    int read = 0;
+
+                    while (read < len)
+                        read += await stream.ReadAsync(data, read, len - read);
+
+                    using (MemoryStream ms = new MemoryStream(data))
+                    {
+                        BitmapImage img = new BitmapImage();
+                        img.BeginInit();
+                        img.StreamSource = ms;
+                        img.CacheOption = BitmapCacheOption.OnLoad;
+                        img.EndInit();
+
+                        Dispatcher.Invoke(() =>
+                        {
+                            ScreenView.Source = img;
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ DesktopControlAgent/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+
+        var client = server.AcceptTcpClient();
+        var stream = client.GetStream();
+
+        while (true)
+        {
+            using (Bitmap bmp = Capture())
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                byte[] data = ms.ToArray();
+                byte[] len = BitConverter.GetBytes(data.Length);
+
+                stream.Write(len, 0, 4);
+                stream.Write(data, 0, data.Length);
+            }
+
+            Thread.Sleep(100);
+        }
+    }
+
+    static Bitmap Capture()
+    {
+        int w = 1920;
+        int h = 1080;
+
+        Bitmap bmp = new Bitmap(w, h);
+
+        using (Graphics g = Graphics.FromImage(bmp))
+        {
+            g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+        }
+
+        return bmp;
+    }
+}
