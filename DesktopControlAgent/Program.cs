29be592ff28db57f747306eca3fdb37d17411aeb
Szymon Sowiński4C_gr1 <4C_gr1@DESKTOP-JKF863E>
2026-04-21 11:45:12 +0200

    Dokumentacja techniczna klasy aplikacji klienckiej agenta

--- DesktopControlAgent/Program.cs
+++ DesktopControlAgent/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*********************
+nazwa pliku: Agent (Program)
+opis: Aplikacja działająca jako agent na komputerze. Umożliwia zdalny podgląd ekranu (port 5000) oraz wykonywanie komend (port 6000, np. shutdown).
+*********************/
+using System;
 using System.Drawing;
 using System.IO;
 using System.Net;
@@ -6,108 +10,126 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
+/*********************
+nazwa klasy: Program
+opis: Główna klasa aplikacji. Uruchamia serwer podglądu ekranu oraz serwer komend.
+*********************/
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Start agenta...");
+	/*********************
+    nazwa metody: Main
+    opis: Punkt startowy aplikacji. Uruchamia dwa wątki: ScreenServer oraz CommandServer.
+    *********************/
+	static void Main()
+	{
+		Console.WriteLine("Start agenta...");
 
-        new Thread(ScreenServer).Start();
-        new Thread(CommandServer).Start();
+		new Thread(ScreenServer).Start();
+		new Thread(CommandServer).Start();
 
-        Console.WriteLine("Działa. ENTER = exit");
-        Console.ReadLine();
-    }
+		Console.WriteLine("Działa. ENTER = exit");
+		Console.ReadLine();
+	}
 
-    // ===== STREAM EKRANU =====
-    static void ScreenServer()
-    {
-        TcpListener server = new TcpListener(IPAddress.Any, 5000);
-        server.Start();
+	/*********************
+    nazwa metody: ScreenServer
+    opis: Serwer TCP wysyłający obraz ekranu do klienta (port 5000). Wysyła kolejne klatki jako JPEG.
+    *********************/
+	static void ScreenServer()
+	{
+		TcpListener server = new TcpListener(IPAddress.Any, 5000);
+		server.Start();
 
-        Console.WriteLine("Screen server (5000) działa...");
+		Console.WriteLine("Screen server (5000) działa...");
 
-        while (true)
-        {
-            var client = server.AcceptTcpClient();
-            Console.WriteLine("Klient podglądu połączony");
+		while (true)
+		{
+			var client = server.AcceptTcpClient();
+			Console.WriteLine("Klient podglądu połączony");
 
-            var stream = client.GetStream();
+			var stream = client.GetStream();
 
-            try
-            {
-                while (true)
-                {
-                    using (Bitmap bmp = Capture())
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+			try
+			{
+				while (true)
+				{
+					using (Bitmap bmp = Capture())
+					using (MemoryStream ms = new MemoryStream())
+					{
+						bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
 
-                        byte[] data = ms.ToArray();
-                        byte[] len = BitConverter.GetBytes(data.Length);
+						byte[] data = ms.ToArray();
+						byte[] len = BitConverter.GetBytes(data.Length);
 
-                        stream.Write(len, 0, 4);
-                        stream.Write(data, 0, data.Length);
-                    }
+						stream.Write(len, 0, 4);
+						stream.Write(data, 0, data.Length);
+					}
 
-                    Thread.Sleep(100);
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Klient podglądu rozłączony");
-                client.Close();
-            }
-        }
-    }
+					Thread.Sleep(100);
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Klient podglądu rozłączony");
+				client.Close();
+			}
+		}
+	}
 
-    // ===== KOMENDY (shutdown) =====
-    static void CommandServer()
-    {
-        TcpListener server = new TcpListener(IPAddress.Any, 6000);
-        server.Start();
+	/*********************
+    nazwa metody: CommandServer
+    opis: Serwer TCP odbierający komendy (port 6000). Obsługuje m.in. komendę "shutdown".
+    *********************/
+	static void CommandServer()
+	{
+		TcpListener server = new TcpListener(IPAddress.Any, 6000);
+		server.Start();
 
-        Console.WriteLine("Command server (6000) działa...");
+		Console.WriteLine("Command server (6000) działa...");
 
-        while (true)
-        {
-            var client = server.AcceptTcpClient();
-            var stream = client.GetStream();
+		while (true)
+		{
+			var client = server.AcceptTcpClient();
+			var stream = client.GetStream();
 
-            byte[] buffer = new byte[1024];
-            int len = stream.Read(buffer, 0, buffer.Length);
+			byte[] buffer = new byte[1024];
+			int len = stream.Read(buffer, 0, buffer.Length);
 
-            string cmd = Encoding.UTF8.GetString(buffer, 0, len);
+			string cmd = Encoding.UTF8.GetString(buffer, 0, len);
 
-            Console.WriteLine("Komenda: " + cmd);
+			Console.WriteLine("Komenda: " + cmd);
 
-            if (cmd == "shutdown")
-            {
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
-                {
-                    FileName = "shutdown",
-                    Arguments = "/s /f /t 0",
-                    CreateNoWindow = true,
-                    UseShellExecute = false
-                });
-            }
+			if (cmd == "shutdown")
+			{
+				System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+				{
+					FileName = "shutdown",
+					Arguments = "/s /f /t 0",
+					CreateNoWindow = true,
+					UseShellExecute = false
+				});
+			}
 
-            client.Close();
-        }
-    }
+			client.Close();
+		}
+	}
 
-    static Bitmap Capture()
-    {
-        int w = 1920;
-        int h = 1080;
+	/*********************
+    nazwa metody: Capture
+    opis: Wykonuje zrzut ekranu i zwraca go jako obiekt Bitmap.
+    *********************/
+	static Bitmap Capture()
+	{
+		int w = 1920;
+		int h = 1080;
 
-        Bitmap bmp = new Bitmap(w, h);
+		Bitmap bmp = new Bitmap(w, h);
 
-        using (Graphics g = Graphics.FromImage(bmp))
-        {
-            g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
-        }
+		using (Graphics g = Graphics.FromImage(bmp))
+		{
+			g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+		}
 
-        return bmp;
-    }
+		return bmp;
+	}
 }
